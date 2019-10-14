3a75112fc173796a077791b797e44614bb886358
SW-Jeon <w2263@daum.net>
2019-10-14 14:21:01 +0900

    a

--- 3_project/src/SW_controller/Homecontroller.java
+++ 3_project/src/SW_controller/Homecontroller.java
@@ -6,7 +6,7 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-@WebServlet("/pro/home")
+@WebServlet("/pro/home1")
 public class Homecontroller extends HttpServlet {
 	@Override
 	protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
